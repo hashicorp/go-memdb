5cfe678b9afd82c22c8f97de1ba20909c195265f
James Phillips <slackpad@gmail.com>
2017-01-19 10:52:32 -0800

    Implements watchMany in terms of watchFew and adds a soft limit add fn.

--- watch.go
+++ watch.go
@@ -2,15 +2,15 @@ package memdb
 
 import "time"
 
-// WatchSet is a collection of watch channels
+// WatchSet is a collection of watch channels.
 type WatchSet map[<-chan struct{}]struct{}
 
-// NewWatchSet constructs a new watch set
+// NewWatchSet constructs a new watch set.
 func NewWatchSet() WatchSet {
 	return make(map[<-chan struct{}]struct{})
 }
 
-// Add appends a watchCh to the WatchSet if non-nil
+// Add appends a watchCh to the WatchSet if non-nil.
 func (w WatchSet) Add(watchCh <-chan struct{}) {
 	if w == nil {
 		return
@@ -21,90 +21,84 @@ func (w WatchSet) Add(watchCh <-chan struct{}) {
 	}
 }
 
-// Watch is used to wait for either the watch set to trigger or a timeout
-// Returns true on timeout
+// AddWithLimit appends a watchCh to the WatchSet if non-nil, and if the given
+// softLimit hasn't been exceeded. Otherwise, it will watch the given alternate
+// channel. It's expected that the altCh will be the same on many calls to this
+// function, so you will exceed the soft limit a little bit if you hit this, but
+// not by much.
+//
+// This is useful if you want to track individual items up to some limit, after
+// which you watch a higher-level channel (usually a channel from start start of
+// an iterator higher up in the radix tree) that will watch a superset of items.
+func (w WatchSet) AddWithLimit(softLimit int, watchCh <-chan struct{}, altCh <-chan struct{}) {
+	// This is safe for a nil WatchSet so we don't need to check that here.
+	if len(w) < softLimit {
+		w.Add(watchCh)
+	} else {
+		w.Add(altCh)
+	}
+}
+
+// Watch is used to wait for either the watch set to trigger or a timeout.
+// Returns true on timeout.
 func (w WatchSet) Watch(timeoutCh <-chan time.Time) bool {
 	if w == nil {
 		return false
 	}
 
-	if n := len(w); n <= 8 {
-		return w.watchFew(timeoutCh)
+	if n := len(w); n <= aFew {
+		idx := 0
+		chunk := make([]<-chan struct{}, aFew)
+		for watchCh := range w {
+			chunk[idx] = watchCh
+			idx++
+		}
+		return watchFew(chunk, timeoutCh)
 	} else {
 		return w.watchMany(timeoutCh)
 	}
 }
 
-// watchFew is used if there are only a few watchers as a performance optimization
-func (w WatchSet) watchFew(timeoutCh <-chan time.Time) bool {
-	var ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8 <-chan struct{}
-	idx := 0
-	for watchCh := range w {
-		switch idx {
-		case 0:
-			ch1 = watchCh
-		case 1:
-			ch2 = watchCh
-		case 2:
-			ch3 = watchCh
-		case 3:
-			ch4 = watchCh
-		case 4:
-			ch5 = watchCh
-		case 5:
-			ch6 = watchCh
-		case 6:
-			ch7 = watchCh
-		case 7:
-			ch8 = watchCh
-		}
-		idx++
-	}
-	select {
-	case <-ch1:
-		return false
-	case <-ch2:
-		return false
-	case <-ch3:
-		return false
-	case <-ch4:
-		return false
-	case <-ch5:
-		return false
-	case <-ch6:
-		return false
-	case <-ch7:
-		return false
-	case <-ch8:
-		return false
-	case <-timeoutCh:
-		return true
-	}
-}
-
-// watchMany is used if there are many watchers
+// watchMany is used if there are many watchers.
 func (w WatchSet) watchMany(timeoutCh <-chan time.Time) bool {
-	doneCh := make(chan struct{})
+	// Make a fake timeout channel we can feed into watchFew to cancel all
+	// the blocking goroutines.
+	doneCh := make(chan time.Time)
 	defer close(doneCh)
 
-	// Start a goroutine for each watcher
+	// Set up a goroutine for each watcher.
 	triggerCh := make(chan struct{}, 1)
-	watcher := func(ch <-chan struct{}) {
-		select {
-		case <-ch:
+	watcher := func(chunk []<-chan struct{}) {
+		if timeout := watchFew(chunk, doneCh); !timeout {
 			select {
 			case triggerCh <- struct{}{}:
 			default:
 			}
-		case <-doneCh:
-			return
 		}
 	}
+
+	// Apportion the watch channels into chunks we can feed into the
+	// watchFew helper.
+	idx := 0
+	chunk := make([]<-chan struct{}, aFew)
 	for watchCh := range w {
-		go watcher(watchCh)
+		subIdx := idx % aFew
+		chunk[subIdx] = watchCh
+		idx++
+
+		// Fire off this chunk and start a fresh one.
+		if idx%aFew == 0 {
+			go watcher(chunk)
+			chunk = make([]<-chan struct{}, aFew)
+		}
 	}
 
-	// Wait for a channel to trigger or timeout
+	// Make sure to watch any residual channels in the last chunk.
+	if idx%aFew != 0 {
+		go watcher(chunk)
+	}
+
+	// Wait for a channel to trigger or timeout.
 	select {
 	case <-triggerCh:
 		return false
