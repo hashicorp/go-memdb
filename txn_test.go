hashicorp/go-memdb

commit 97e94de6e70b3bcdd50849897f5e601e37055947

fix: allow nil string pointers
If a string pointer field is nil, do no return an error but an empty
value.

This helps for the cases when AllowMissing for a field is okay but we
would still like it to be indexed.

index.go
@@ -63,10 +63,16 @@ func (s *StringFieldIndex) FromObject(obj interface{}) (bool, []byte, error) {
 	v = reflect.Indirect(v) // Dereference the pointer if any
 
 	fv := v.FieldByName(s.Field)
+	isPtr := fv.Kind() == reflect.Ptr
 	fv = reflect.Indirect(fv)
-	if !fv.IsValid() {
+	if !isPtr && !fv.IsValid() {
 		return false, nil,
-			fmt.Errorf("field '%s' for %#v is invalid", s.Field, obj)
+			fmt.Errorf("field '%s' for %#v is invalid %v ", s.Field, obj, isPtr)
+	}
+
+	if isPtr && !fv.IsValid() {
+		val := ""
+		return true, []byte(val), nil
 	}
 
 	val := fv.String()

index_test.go
@@ -14,6 +14,7 @@ type TestObject struct {
 	ID       string
 	Foo      string
 	Fu       *string
+	Boo      *string
 	Bar      int
 	Baz      string
 	Bam      *bool
@@ -39,6 +40,7 @@ func testObj() *TestObject {
 		ID:  "my-cool-obj",
 		Foo: "Testing",
 		Fu:  String("Fu"),
+		Boo: nil,
 		Bar: 42,
 		Baz: "yep",
 		Bam: &b,
@@ -105,6 +107,18 @@ func TestStringFieldIndex_FromObject(t *testing.T) {
 		t.Fatalf("err: %v", err)
 	}
 	if string(val) != "Fu\x00" {
+		t.Fatalf("bad: %s", val)
+	}
+	if !ok {
+		t.Fatalf("should be ok")
+	}
+
+	pointerField = StringFieldIndex{"Boo", false}
+	ok, val, err = pointerField.FromObject(obj)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if string(val) != "" {
 		t.Fatalf("bad: %s", val)
 	}
 	if !ok {

commit 92e457f524d8445ad7fc747fbc596aaa6c0a898e

fix: nil strings should be allowed only when AllowMissing is set
This corrects a bug introduced in
97e94de6e70b3bcdd50849897f5e601e37055947.

If pointer to strings are being Indexed, a nil pointer should not result
in an error only when AllowMissing is set to true for an Index,
otherwise it should rightfully error.

index.go
@@ -73,7 +73,7 @@ func (s *StringFieldIndex) FromObject(obj interface{}) (bool, []byte, error) {
 
 	if isPtr && !fv.IsValid() {
 		val := ""
-		return true, []byte(val), nil
+		return false, []byte(val), nil
 	}
 
 	val := fv.String()

index_test.go
@@ -131,8 +131,8 @@ func TestStringFieldIndex_FromObject(t *testing.T) {
 	if string(val) != "" {
 		t.Fatalf("bad: %s", val)
 	}
-	if !ok {
-		t.Fatalf("should be ok")
+	if ok {
+		t.Fatalf("should be not ok")
 	}
 }
 

txn_test.go
@@ -1217,6 +1217,7 @@ func TestTxn_LowerBound(t *testing.T) {
 
 			txn := db.Txn(true)
 			for _, row := range tc.Rows {
+
 				err := txn.Insert("main", row)
 				if err != nil {
 					t.Fatalf("err inserting: %s", err)
@@ -1242,4 +1243,114 @@ func TestTxn_LowerBound(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestStringFieldIndexerEmptyPointerFromArgs(t *testing.T) {
+	t.Run("does not error with AllowMissing", func(t *testing.T) {
+		schema := &DBSchema{
+			Tables: map[string]*TableSchema{
+				"main": &TableSchema{
+					Name: "main",
+					Indexes: map[string]*IndexSchema{
+						"id": &IndexSchema{
+							Name:   "id",
+							Unique: true,
+							Indexer: &StringFieldIndex{
+								Field: "ID",
+							},
+						},
+						"fu": &IndexSchema{
+							Name: "fu",
+							Indexer: &StringFieldIndex{
+								Field: "Fu",
+							},
+							AllowMissing: true,
+						},
+					},
+				},
+			},
+		}
+
+		db, err := NewMemDB(schema)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		txn := db.Txn(true)
+
+		s1 := "foo1"
+		obj1 := &TestObject{
+			ID: "object1",
+			Fu: &s1,
+		}
+
+		obj2 := &TestObject{
+			ID: "object2",
+			Fu: nil,
+		}
+
+		err = txn.Insert("main", obj1)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		err = txn.Insert("main", obj2)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	})
+
+	t.Run("errors without AllowMissing", func(t *testing.T) {
+		schema := &DBSchema{
+			Tables: map[string]*TableSchema{
+				"main": &TableSchema{
+					Name: "main",
+					Indexes: map[string]*IndexSchema{
+						"id": &IndexSchema{
+							Name:   "id",
+							Unique: true,
+							Indexer: &StringFieldIndex{
+								Field: "ID",
+							},
+						},
+						"fu": &IndexSchema{
+							Name: "fu",
+							Indexer: &StringFieldIndex{
+								Field: "Fu",
+							},
+							AllowMissing: false,
+						},
+					},
+				},
+			},
+		}
+
+		db, err := NewMemDB(schema)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		txn := db.Txn(true)
+
+		s1 := "foo1"
+		obj1 := &TestObject{
+			ID: "object1",
+			Fu: &s1,
+		}
+
+		obj2 := &TestObject{
+			ID: "object2",
+			Fu: nil,
+		}
+
+		err = txn.Insert("main", obj1)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		err = txn.Insert("main", obj2)
+		if err == nil {
+			t.Fatalf("expected err not to be nil")
+		}
+	})
 }