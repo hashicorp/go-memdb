2cbc5c5894d05bf2d030e6e01c95c68881b98c33
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-05-20 23:40:38 +0530

    Using ART

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/hashicorp/go-memdb
 
-go 1.13
+go 1.21
 
-require (
-	github.com/hashicorp/go-immutable-radix v1.3.0
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-)
+toolchain go1.22.2
+
+require github.com/absolutelightning/go-immutable-adaptive-radix v1.0.4
+
+require github.com/hashicorp/golang-lru v0.5.4 // indirect
--- txn.go
+++ txn.go
@@ -6,11 +6,10 @@ package memdb
 import (
 	"bytes"
 	"fmt"
+	adaptive "github.com/absolutelightning/go-immutable-adaptive-radix"
 	"strings"
 	"sync/atomic"
 	"unsafe"
-
-	iradix "github.com/hashicorp/go-immutable-radix"
 )
 
 const (
@@ -33,14 +32,14 @@ type tableIndex struct {
 type Txn struct {
 	db      *MemDB
 	write   bool
-	rootTxn *iradix.Txn
+	rootTxn *adaptive.Txn[any]
 	after   []func()
 
 	// changes is used to track the changes performed during the transaction. If
 	// it is nil at transaction start then changes are not tracked.
 	changes Changes
 
-	modified map[tableIndex]*iradix.Txn
+	modified map[tableIndex]*adaptive.Txn[any]
 }
 
 // TrackChanges enables change tracking for the transaction. If called at any
@@ -58,7 +57,7 @@ func (txn *Txn) TrackChanges() {
 // readableIndex returns a transaction usable for reading the given index in a
 // table. If the transaction is a write transaction with modifications, a clone of the
 // modified index will be returned.
-func (txn *Txn) readableIndex(table, index string) *iradix.Txn {
+func (txn *Txn) readableIndex(table, index string) *adaptive.Txn[any] {
 	// Look for existing transaction
 	if txn.write && txn.modified != nil {
 		key := tableIndex{table, index}
@@ -71,15 +70,15 @@ func (txn *Txn) readableIndex(table, index string) *iradix.Txn {
 	// Create a read transaction
 	path := indexPath(table, index)
 	raw, _ := txn.rootTxn.Get(path)
-	indexTxn := raw.(*iradix.Tree).Txn()
+	indexTxn := raw.(*adaptive.RadixTree[any]).Txn()
 	return indexTxn
 }
 
 // writableIndex returns a transaction usable for modifying the
 // given index in a table.
-func (txn *Txn) writableIndex(table, index string) *iradix.Txn {
+func (txn *Txn) writableIndex(table, index string) *adaptive.Txn[any] {
 	if txn.modified == nil {
-		txn.modified = make(map[tableIndex]*iradix.Txn)
+		txn.modified = make(map[tableIndex]*adaptive.Txn[any])
 	}
 
 	// Look for existing transaction
@@ -92,7 +91,7 @@ func (txn *Txn) writableIndex(table, index string) *iradix.Txn {
 	// Start a new transaction
 	path := indexPath(table, index)
 	raw, _ := txn.rootTxn.Get(path)
-	indexTxn := raw.(*iradix.Tree).Txn()
+	indexTxn := raw.(*adaptive.RadixTree[any]).Txn()
 
 	// If we are the primary DB, enable mutation tracking. Snapshots should
 	// not notify, otherwise we will trigger watches on the primary DB when
@@ -647,7 +646,7 @@ func (txn *Txn) LongestPrefix(table, index string, args ...interface{}) (interfa
 
 	// Find the longest prefix match with the given index.
 	indexTxn := txn.readableIndex(table, indexSchema.Name)
-	if _, value, ok := indexTxn.Root().LongestPrefix(val); ok {
+	if _, value, ok := indexTxn.LongestPrefix(val); ok {
 		return value, nil
 	}
 	return nil, nil
@@ -923,7 +922,7 @@ func (txn *Txn) Changes() Changes {
 	return cs
 }
 
-func (txn *Txn) getIndexIterator(table, index string, args ...interface{}) (*iradix.Iterator, []byte, error) {
+func (txn *Txn) getIndexIterator(table, index string, args ...interface{}) (*adaptive.Iterator[any], []byte, error) {
 	// Get the index value to scan
 	indexSchema, val, err := txn.getIndexValue(table, index, args...)
 	if err != nil {
@@ -939,7 +938,7 @@ func (txn *Txn) getIndexIterator(table, index string, args ...interface{}) (*ira
 	return indexIter, val, nil
 }
 
-func (txn *Txn) getIndexIteratorReverse(table, index string, args ...interface{}) (*iradix.ReverseIterator, []byte, error) {
+func (txn *Txn) getIndexIteratorReverse(table, index string, args ...interface{}) (*adaptive.ReverseIterator[any], []byte, error) {
 	// Get the index value to scan
 	indexSchema, val, err := txn.getIndexValue(table, index, args...)
 	if err != nil {
@@ -963,11 +962,11 @@ func (txn *Txn) Defer(fn func()) {
 	txn.after = append(txn.after, fn)
 }
 
-// radixIterator is used to wrap an underlying iradix iterator.
+// radixIterator is used to wrap an underlying adaptive iterator.
 // This is much more efficient than a sliceIterator as we are not
 // materializing the entire view.
 type radixIterator struct {
-	iter    *iradix.Iterator
+	iter    *adaptive.Iterator[any]
 	watchCh <-chan struct{}
 }
 
@@ -984,7 +983,7 @@ func (r *radixIterator) Next() interface{} {
 }
 
 type radixReverseIterator struct {
-	iter    *iradix.ReverseIterator
+	iter    *adaptive.ReverseIterator[any]
 	watchCh <-chan struct{}
 }
 
