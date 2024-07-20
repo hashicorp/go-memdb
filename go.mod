8c883ef00a4ca8e43aef319da20bee0f505a2eba
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-07-20 17:45:43 +0530

    using iradix 1.0.8

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.22.4
 
 toolchain go1.22.5
 
-require github.com/absolutelightning/go-immutable-radix v1.0.7
+require github.com/absolutelightning/go-immutable-radix v1.0.8
 
 require github.com/hashicorp/golang-lru v0.5.0 // indirect
--- txn.go
+++ txn.go
@@ -72,7 +72,7 @@ func (txn *Txn) readableIndex(table, index string) *iradix.Txn {
 	path := indexPath(table, index)
 	raw, _ := txn.rootTxn.Get(path)
 	tree := raw.(*iradix.Tree)
-	tree.Root().SetSnapShotNode(txn.rootTxn.Root().GetSnapshotNode())
+	tree.Root().SetSnapshot(txn.rootTxn.Root().GetSnapshot())
 	indexTxn := tree.Txn()
 	return indexTxn
 }
@@ -95,7 +95,7 @@ func (txn *Txn) writableIndex(table, index string) *iradix.Txn {
 	path := indexPath(table, index)
 	raw, _ := txn.rootTxn.Get(path)
 	tree := raw.(*iradix.Tree)
-	tree.Root().SetSnapShotNode(txn.rootTxn.Root().GetSnapshotNode())
+	tree.Root().SetSnapshot(txn.rootTxn.Root().GetSnapshot())
 	indexTxn := tree.Txn()
 
 	// If we are the primary DB, enable mutation tracking. Snapshots should
