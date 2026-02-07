ee471116fda691513636db5c33b41b4d7757f718
Akshay Nehe <akshaadnehe@gmail.com>
2026-02-06 23:29:20 -0800

    Introducing Count for Index column values

--- memdb_test.go
+++ memdb_test.go
@@ -86,3 +86,113 @@ func TestMemDB_Snapshot(t *testing.T) {
 		t.Fatalf("should exist")
 	}
 }
+
+func TestMemDB_SimpleCount(t *testing.T) {
+	db, err := NewMemDB(testValidSchema())
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	txn := db.Txn(true)
+
+	obj1 := &TestObject{
+		ID:  "a",
+		Foo: "xyz",
+		Qux: []string{"xyz1"},
+	}
+	obj2 := &TestObject{
+		ID:  "medium-length",
+		Foo: "xyz",
+		Qux: []string{"xyz1", "xyz2"},
+	}
+	obj3 := &TestObject{
+		ID:  "super-long-unique-identifier",
+		Foo: "xyz",
+		Qux: []string{"xyz1", "xyz2"},
+		Bar: 2,
+	}
+
+	// Pre-insert counts should be zero
+	if count := db.Count("main", "id"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "foo"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "qux"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	err = txn.Insert("main", obj1)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Pre-commit counts should be zero
+	if count := db.Count("main", "id"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "foo"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "qux"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	err = txn.Insert("main", obj2)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	txn.Commit()
+
+	// Post-commit counts should reflect the two inserted objects
+	if count := db.Count("main", "id"); count != 2 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "foo"); count != 2 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	// The "qux" index should have 3 entries because obj2 has two values in the "qux" field
+	if count := db.Count("main", "qux"); count != 3 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	txn = db.Txn(true)
+	err = txn.Insert("main", obj3)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	txn.Commit()
+
+	txn = db.Txn(false)
+	defer txn.Abort()
+
+	if count := db.Count("main", "id"); count != 3 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "foo"); count != 3 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	if count := db.Count("main", "qux"); count != 5 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	// non-index column not supported by Count and will return 0
+	if count := db.Count("main", "bar"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+
+	// non-existent column not supported by Count and will return 0
+	if count := db.Count("main", "bazooka"); count != 0 {
+		t.Fatalf("bad count: %d", count)
+	}
+}
